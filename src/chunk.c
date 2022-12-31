630a3860e4789298e173238ea7c39d6b37dff578
0xsuk <97814789+0xsuk@users.noreply.github.com>
2023-01-01 00:21:47 +0900

    HELLO 2023

--- src/chunk.c
+++ src/chunk.c
@@ -5,10 +5,12 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
 
@@ -24,3 +26,7 @@ void writeChunk(Chunk *chunk, uint8_t byte) {
   chunk->count++;
 }
 
+int addConstant(Chunk *chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
+}
