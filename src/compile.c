d969fd300fdbaa601b91a0e99bfb05527beaab14
0xsuk <97814789+0xsuk@users.noreply.github.com>
2023-01-05 19:30:32 +0900

    error syncronization

--- src/compile.c
+++ src/compile.c
@@ -219,9 +219,37 @@ static void printStatement() {
   emitByte(OP_PRINT);
 }
 
+// move to the next statement
+static void synchronize() {
+  parser.panicMode = false;
+
+  while (parser.current.type != TOKEN_EOF) {
+    if (parser.previous.type == TOKEN_SEMICOLON)
+      return;
+    switch (parser.current.type) {
+    case TOKEN_CLASS:
+    case TOKEN_FUN:
+    case TOKEN_VAR:
+    case TOKEN_FOR:
+    case TOKEN_IF:
+    case TOKEN_WHILE:
+    case TOKEN_PRINT:
+    case TOKEN_RETURN:
+      return;
+
+    default:;
+    }
+    advance();
+  }
+}
+
 static void statement();
 
-static void declaration() { statement(); }
+static void declaration() {
+  statement();
+  if (parser.panicMode)
+    synchronize();
+}
 
 static void statement() {
   if (match(TOKEN_PRINT)) {
